a2f5e02253a77371e991928482977f9e137eaf65
Ville Pelimanni <ville.pelimanni@gmail.com>
2022-10-03 17:03:16 +0300

    Palautettava versio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [age , setAge] =useState(0);
+  const [limitUpper , setUpper] =useState(1);
+  const [limitLower , setLower] =useState(2);
+
+  function calculate () {
+    setUpper ((220 - age) * 0.85);
+    setLower ((220 - age) * 0.65);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h3 class="pt-2">Heart rate limits calculator</h3>
+      <form>
+        <div class="py-2">
+          <label>Age </label>
+          <input type="number" class="form-control" placeholder="Enter age" value={age} onChange={e => setAge(e.target.value)} />
+        </div>
+        <div class="pb-2">
+          <label>Heart rate limits </label>
+          <output class="form-control">{limitLower.toFixed(2)} - {limitUpper.toFixed(3)}</output>
+        </div>
+        <button type="button" class="btn btn-primary" onClick={calculate}>Calculate</button>
+      </form>
     </div>
   );
 }
